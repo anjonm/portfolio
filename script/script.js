dacc523815f11796e296fafb814df9bb7c9ad251
Anjon <marianoanjon@gmail.com>
2025-01-30 13:10:54 +0800

    navigation mobile
    burger click eventlistener

--- script/script.js
+++ script/script.js
@@ -1,7 +1,13 @@
-const navbar = document.querySelector('#navbar');
+const burger = document.querySelector('#burger');
 const menu = document.querySelector('#burger+ul');
 
-navbar.addEventListener('click', function () {
+navbar.addEventListener('click', function (e) {
 	menu.classList.toggle('show');
-	// alert('wah');
+	e.stopPropagation();
+});
+
+document.addEventListener('click', function (e) {
+	if (!burger.contains(e.target)) {
+		menu.classList.remove('show');
+	}
 });
